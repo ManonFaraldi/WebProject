0c91317734ee819f5c9f275168bf918bb3e9431c
Manon Faraldi <mfaraldi@ensc.fr>
2018-03-24 17:13:37 +0100

    gcu

---
+++ Code-projetWeb_v2/Code_projetV2/pages_php/gcu.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once "../includes/functions.php"; 
+require_once "../includes/header.php";
+?>
+
+<main role="main" class="container">
+   
+        <h1 style="opacity: 0; display:none;"> Génération Génération-s Générations Benoit Hamon M1717 Mouvement Mouvement Génération </h1>
+    
+    <h1>CGU</h1>
+    
+        <section id="header-map" class="donation-content">
+
+<h2 class="">Conditions G&eacute;n&eacute;rales d'Utilisation</h2>
+<p class="article-full__body-content">L&rsquo;utilisateur est inform&eacute; que, lors de sa navigation sur le présent site&nbsp;des donn&eacute;es &agrave; caract&egrave;re personnel peuvent lui &ecirc;tre demand&eacute;es via un formulaire &agrave; remplir notamment en cas d&rsquo;abonnement &agrave; la newsletter, d&rsquo;adh&eacute;sion, ou s'il souhaite apporter sa contribution sur le site. L&rsquo;utilisateur est inform&eacute; que ses donn&eacute;es sont collect&eacute;es pour des finalit&eacute;s d&eacute;termin&eacute;es et l&eacute;gitimes, qu&rsquo;elles ne seront en aucun cas transmises &agrave; des tiers et qu&rsquo;elles sont conserv&eacute;es pendant une dur&eacute;e qui n&rsquo;exc&egrave;de pas la dur&eacute;e n&eacute;cessaire aux finalit&eacute;s pour lesquelles elles sont collect&eacute;es et trait&eacute;es.</p>
+<p>&nbsp;</p>
+<p>ETEE | 13 | Ter | rue du Bel Air | 78190 TRAPPES</p>
+<p>&nbsp;</p>
+<p>Toutes les pr&eacute;cautions sont prises pour pr&eacute;server la s&eacute;curit&eacute; des donn&eacute;es, et notamment pour &eacute;viter qu&rsquo;elles ne soient d&eacute;form&eacute;es, endommag&eacute;es ou que des tiers non autoris&eacute;s y aient acc&egrave;s. Conform&eacute;ment &agrave; la loi modifi&eacute;e relative &agrave; l&rsquo;informatique, aux fichiers et aux libert&eacute;s du 6 janvier 1978, vous b&eacute;n&eacute;ficiez d&rsquo;un droit d&rsquo;opposition (article 38), d&rsquo;acc&egrave;s (article 39) et de rectification (article 40) aux informations qui vous concernent. Si vous souhaitez exercer ce droit et obtenir communication des informations vous concernant, veuillez vous adresser &agrave; ETEE &agrave; l&rsquo;adresse pr&eacute;-cit&eacute;e.</p>
+<p>&nbsp;</p>
+
+</section>
+
+<?php require_once "../includes/footer.php"; ?>
