fd1fa0979c20b9d12d52cd6c9f4c2054dab82907
Manon Faraldi <mfaraldi@ensc.fr>
2018-03-25 13:35:21 +0200

    presentation

---
+++ Code-projetWeb_v2/Code_projetV2/pages_php/accueil.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once "../includes/functions.php"; 
+require_once "../includes/header.php";
+?>
+
+<h1> Quel monde laisserons-nous aux générations qui viennent ?</h1>
+
+<?php require_once "../includes/footer.php"; ?>
--- Code-projetWeb_v2/Code_projetV2/pages_php/presentation.php
+++ Code-projetWeb_v2/Code_projetV2/pages_php/presentation.php
@@ -3,12 +3,28 @@ session_start();
 require_once "../includes/functions.php"; 
 require_once "../includes/header.php";
 ?>
-
-<div class="container">
-<h1> Presentation </h1>
+<div class="container" >
+    <h1> Presentation </h1>
+</div>
+<hr> 
 <br/>
-<br/>
+<div class="container" >
+    <p> Bienvenue sur le site les Jeunes Génération.s Gironde ! 
+         <br/> Pensez à vous inscrire pour profiter de toutes les possibilités du site et n'hésitez pas à nous rejoindre sur les réseaux sociaux.
+        Et pourquoi ne pas se rencontrer au cours de nos différentes actions que vous
+        trouverez <a href = "../pages_php/actions.php"> ici <span class="glyphicon glyphicon-bullhorn"></span></a>.
+    </p>
 </div>
 
+<div class="container" >
+    <h1> Le comité de l'IEP de Bordeaux </h1>
+</div>
+<hr> 
+<div class="container" >
+    <p> 
+        bla bla bla
+    </p>
+</div>
+<br/>
 
 <?php require_once "../includes/footer.php"; ?>
